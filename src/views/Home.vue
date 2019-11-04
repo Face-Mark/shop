f5dbd56c9180c1b18445a2e171bb6f63cf38761e
huge <2679724735@qq.com>
2019-11-04 10:00:56 +0800

    aside静态效果完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,66 @@
       </div>
     </el-header>
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <!-- 侧边栏 -->
+      <el-aside width="200px">
+        <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b">
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>用户管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="1-1"><i class="el-icon-menu"></i>用户列表</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+       <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>权限管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="2-1"><i class="el-icon-menu"></i>用户列表</el-menu-item>
+          <el-menu-item index="2-2"><i class="el-icon-menu"></i>权限列表</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+       <el-submenu index="3">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>商品管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="3-1"><i class="el-icon-menu"></i>商品列表</el-menu-item>
+          <el-menu-item index="3-2"><i class="el-icon-menu"></i>分类参数</el-menu-item>
+          <el-menu-item index="3-3"><i class="el-icon-menu"></i>商品分类</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+       <el-submenu index="4">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>订单管理</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="4-1"><i class="el-icon-menu"></i>订单列表</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+       <el-submenu index="5">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span>数据统计</span>
+        </template>
+        <el-menu-item-group>
+          <el-menu-item index="5-1"><i class="el-icon-menu"></i>数据报表</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
+      </el-aside>
       <el-main>Main</el-main>
     </el-container>
   </el-container>
@@ -28,6 +87,12 @@ export default {
       }).catch(() => {
         this.$message.info('取消退出')
       })
+    },
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
     }
   }
 }
